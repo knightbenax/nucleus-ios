4bbd63e9a17b222ecb87a4b6fc3c92638663870f
Bezaleel Ashefor <knightbenax@gmail.com>
2017-10-27 10:10:20 +0100

    Initial Commit

---
+++ NucleusTests/NucleusTests.swift
@@ -0,0 +1,36 @@
+//
+//  NucleusTests.swift
+//  NucleusTests
+//
+//  Created by Bezaleel Ashefor on 27/10/2017.
+//  Copyright © 2017 Ephod. All rights reserved.
+//
+
+import XCTest
+@testable import Nucleus
+
+class NucleusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
